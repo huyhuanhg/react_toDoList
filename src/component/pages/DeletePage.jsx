6f0e03cec078ccaba8a83c142b329665f34b6fce
Le Ba Thanh Tuan <le.ba.thanh.tuan@sun-asterisk.com>
2021-07-22 01:21:09 +0700

    Fix Bug Reload Update/Delete

--- src/component/pages/DeletePage.jsx
+++ src/component/pages/DeletePage.jsx
@@ -1,46 +1,55 @@
-import '../../css/alert.css';
-import '../../css/task-list-btn.css';
+import "../../css/alert.css";
+import "../../css/task-list-btn.css";
 
-import {Button, Space} from "antd";
+import { Button, Space } from "antd";
 
 import history from "../../utils/history";
-import { Redirect } from "react-router-dom";
 
-function DeletePage({computedMatch, deleteTask, getTask}) {
-    let k = parseInt(computedMatch.params.key);
+function DeletePage({ computedMatch, deleteTask, taskList }) {
+  const taskKey = computedMatch.params.key;
+  const taskData = taskList.find(
+    (taskItem) => taskItem.key === parseInt(taskKey)
+  );
 
-    if (!getTask(k)){
-        return (<Redirect to={'/'}/>);
-    }
-
-    const { taskName, status, startDate } = getTask(k);
-    return (
-        <div className="alert-delete">
-            <h2>Bạn chắc chắn muốn xóa:</h2>
-            <hr/>
-            <div className="alert-content">
-                <p><span>Tên công việc:</span> {taskName}</p>
-                <p><span>Ngày bắt đầu:</span> {startDate}</p>
-                <p><span>Tình trạng:</span> {status ? "Đã hoàn thành" : "Đang thực hiện"}</p>
-            </div>
-            <div className="alert-control">
-                <Space>
-                    <Button type="primary delete btn-custom" onClick={()=>{
-                        deleteTask(k);
-                        return history.push('/');
-                    }}>
-                        Xóa
-                    </Button>
-                    <Button type="primary btn-custom" onClick={()=>{
-                        history.push('/');
-                    }}>
-                        Hủy
-                    </Button>
-                </Space>
-            </div>
-        </div>
-
-    );
+  return (
+    <div className="alert-delete">
+      <h2>Bạn chắc chắn muốn xóa:</h2>
+      <hr />
+      <div className="alert-content">
+        <p>
+          <span>Tên công việc:</span> {taskData?.taskName}
+        </p>
+        <p>
+          <span>Ngày bắt đầu:</span> {taskData?.startDate}
+        </p>
+        <p>
+          <span>Tình trạng:</span>{" "}
+          {taskData?.status ? "Đã hoàn thành" : "Đang thực hiện"}
+        </p>
+      </div>
+      <div className="alert-control">
+        <Space>
+          <Button
+            type="primary delete btn-custom"
+            onClick={() => {
+              deleteTask(taskKey);
+              return history.push("/");
+            }}
+          >
+            Xóa
+          </Button>
+          <Button
+            type="primary btn-custom"
+            onClick={() => {
+              history.push("/");
+            }}
+          >
+            Hủy
+          </Button>
+        </Space>
+      </div>
+    </div>
+  );
 }
 
 export default DeletePage;
--- src/component/pages/task_form/TaskForm.jsx
+++ src/component/pages/task_form/TaskForm.jsx
@@ -1,103 +1,122 @@
-import {Form, Input, InputNumber, Button, DatePicker, Select} from 'antd';
+import { useEffect } from "react";
+import { Form, Input, Button, DatePicker, Select } from "antd";
 import moment from "moment";
 
-import history from '../../../utils/history';
+import history from "../../../utils/history";
 
+import "../../../css/form.css";
 
-import '../../../css/form.css';
-import {Redirect} from "react-router-dom";
+function TaskForm({
+  createTask,
+  createKey,
+  updateTask,
+  type,
+  computedMatch,
+  taskList,
+}) {
+  const taskKey = type === 'update' && computedMatch.params.key;
+  const taskData = type === 'update' && taskList.find((taskItem) => taskItem.key === parseInt(taskKey));
 
-function TaskForm({createTask, createKey, updateTask, type, computedMatch, getTask}) {
+  const [modifyTaskForm] = Form.useForm();
 
-    if (type === 'update'){
-        let k = parseInt(computedMatch.params.key);
-        if (!getTask(k)){
-            return (<Redirect to={'/'}/>);
-        }
+  useEffect(() => {
+    if (taskData) {
+      modifyTaskForm.resetFields();
     }
+  }, [taskData]);
 
-    let handleStartDate = (startDate) => {
-        if (!startDate) {
-            let today = new Date();
-            return `${today.getFullYear()}-${today.getMonth() + 1}-${today.getDate()}`;
-        } else {
-            return `${startDate.year()}-${startDate.month() + 1}-${startDate.date()}`;
-        }
+  const handleStartDate = (startDate) => {
+    if (startDate) {
+      return moment(startDate).format('YYYY-MM-DD');
+    } else {
+      return moment().format('YYYY-MM-DD');
     }
+  };
 
-    const handleSubmit = ({startDate, ...values}) => {
-        startDate = handleStartDate(startDate);
-        if (type === 'create') {
-            createTask({
-                ...values,
-                startDate
-            });
-        }
-        if (type === 'update') {
-            updateTask({
-                ...values,
-                startDate
-            })
-        }
-        return history.push('/');
+  const handleSubmit = ({ startDate, ...values }) => {
+    const newStartDate = handleStartDate(startDate);
+    if (type === "create") {
+      createTask({
+        ...values,
+        startDate: newStartDate,
+      });
     }
-
-    const setInitialValues = () => {
-        if (type === 'create') {
-            return {
-                status: 0,
-                key: createKey()
-            }
-        } else {
-            let k = parseInt(computedMatch.params.key);
-            const { taskName, status, startDate } = getTask(k);
-            return {
-                key: k,
-                status,
-                taskName,
-                startDate: moment(startDate)
-            }
-        }
+    if (type === "update") {
+      updateTask({
+        ...values,
+        startDate: newStartDate,
+      });
     }
+    return history.push("/");
+  };
 
-    return (
-        <Form labelCol={{span: 6}}
-              wrapperCol={{span: 16}}
-              name="taskForm"
-              className="task-form"
-              onFinish={handleSubmit}
-              initialValues={setInitialValues()}
+  const setInitialValues = () => {
+    if (type === "create") {
+      return {
+        status: 0,
+        key: createKey(),
+      };
+    } else {
+      if (taskData) {
+        const { taskName, status, startDate } = taskData;
+        return {
+          key: parseInt(taskKey),
+          status,
+          taskName,
+          startDate: moment(startDate),
+        };
+      }
+    }
+  };
+
+  return (
+    <Form
+      labelCol={{ span: 6 }}
+      form={modifyTaskForm}
+      wrapperCol={{ span: 16 }}
+      name="taskForm"
+      className="task-form"
+      onFinish={handleSubmit}
+      initialValues={setInitialValues()}
+    >
+      <h1 style={{ textAlign: "center" }}>
+        {type === "create" ? "Thêm công việc" : "Sửa công việc"}
+      </h1>
+      <Form.Item style={{ display: "none" }} name="key">
+        <Input hidden />
+      </Form.Item>
+      <Form.Item
+        name="taskName"
+        label="Tên công việc"
+        rules={[
+          {
+            required: true,
+            message: "Vui lòng nhập tên công việc!",
+          },
+        ]}
+      >
+        <Input placeholder="Tên công việc" />
+      </Form.Item>
+      <Form.Item label="Ngày bắt đầu" name="startDate">
+        <DatePicker style={{ width: "100%" }} placeholder="Ngày bắt đầu" />
+      </Form.Item>
+      <Form.Item label="Trạng thái" name="status">
+        <Select style={{ width: "100%" }}>
+          <Select.Option value={1}>Đã hoàn thành</Select.Option>
+          <Select.Option value={0}>Đang thực hiện</Select.Option>
+        </Select>
+      </Form.Item>
+      <Form.Item>
+        <Button
+          type="primary"
+          htmlType="submit"
+          style={{ width: "40%", marginLeft: "55%" }}
         >
-            <h1 style={{textAlign: "center"}}>{type === 'create' ? 'Thêm công việc' : "Sửa công việc"}</h1>
-            <Form.Item style={{display: "none"}} name="key">
-                <Input hidden/>
-            </Form.Item>
-            <Form.Item name="taskName" label="Tên công việc"
-                       rules={[
-                           {
-                               required: true,
-                               message: "Vui lòng nhập tên công việc!"
-                           }
-                       ]}
-            >
-                <Input placeholder="Tên công việc"/>
-            </Form.Item>
-            <Form.Item label="Ngày bắt đầu" name="startDate">
-                <DatePicker style={{width: "100%"}} placeholder="Ngày bắt đầu"/>
-            </Form.Item>
-            <Form.Item label="Trạng thái" name="status">
-                <Select style={{width: "100%"}}>
-                    <Select.Option value={1}>Đã hoàn thành</Select.Option>
-                    <Select.Option value={0}>Đang thực hiện</Select.Option>
-                </Select>
-            </Form.Item>
-            <Form.Item>
-                <Button type="primary" htmlType="submit" style={{width: "40%", marginLeft: '55%'}}>
-                    {type === 'create' ? 'Thêm' : "Sửa"}
-                </Button>
-            </Form.Item>
-        </Form>
-    );
+          {type === "create" ? "Thêm" : "Sửa"}
+        </Button>
+      </Form.Item>
+    </Form>
+  );
 }
 
 export default TaskForm;
